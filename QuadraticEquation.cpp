2e7274016527bcebe4003abc55c95a2e5707aaf9
Victor Leskin <victorleskin16@gmail.com>
2025-04-12 12:53:51 +0300

    Add empty class for quadratic equation

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -1,5 +1,29 @@
 #include <gtest/gtest.h>
 
+class QuadraticEquation
+{
+};
+
+// gTest grouping class
+class test_QuadraticEquation : public ::testing::Test
+{
+public:
+    // additional class to access to member of tested class
+    class Test_QuadraticEquation : public QuadraticEquation
+    {
+    public:
+        // add here members for free access.
+        using QuadraticEquation::QuadraticEquation; // delegate constructors
+    };
+
+};
+
+TEST_F(test_QuadraticEquation, test_ctor)
+{
+    Test_QuadraticEquation t;
+}
+
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
