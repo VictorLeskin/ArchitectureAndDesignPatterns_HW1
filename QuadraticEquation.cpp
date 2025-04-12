dcdd4989951c823c430c7bae915f430d597ebab6
Victor Leskin <victorleskin16@gmail.com>
2025-04-12 14:53:35 +0300

    Add test case and code for equation with 2 roots.

--- QuadraticEquation.cpp
+++ QuadraticEquation.cpp
@@ -4,14 +4,22 @@
 
 class QuadraticEquation
 {
+
     public:
+    inline static const double eps = 1.0e-15;
+
     static std::tuple<int, double, double> solve(double a, double b, double c)
     {
         double D = b * b - 4 * a * c;
-        if (D < 0)
+        if (D < -eps)
             return std::tuple(0,0,0);
 
-        return std::tuple(99, 0, 0);
+        a *= 2;
+        if( D > eps )
+        {
+            D = sqrt(D);
+            return std::tuple(2, (-b - D) / a, (-b + D) / a);
+        }
     }
 };
 
@@ -29,18 +37,19 @@ public:
     };
 };
 
-TEST_F(test_QuadraticEquation, test_solve_no_roots )
+TEST_F(test_QuadraticEquation, test_solve_no_roots)
 {
     auto res = Test_QuadraticEquation::solve(1, 0, 1);
 
     EXPECT_EQ(0, std::get<0>(res));
 }
 
+TEST_F(test_QuadraticEquation, test_solve_2_roots)
+{
+    auto res = Test_QuadraticEquation::solve(1, 0, -1);
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(2, std::get<0>(res));
+    EXPECT_NEAR(-1, std::get<1>(res), 1.0e-15);
+    EXPECT_NEAR( 1, std::get<2>(res), 1.0e-15);
+
 }
